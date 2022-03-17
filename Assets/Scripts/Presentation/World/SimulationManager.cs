8fd614f3b3a7f44c5137888fe4a9efef22543f31
Horváth István <dzctir@inf.elte.hu>
2022-03-17 20:48:45 +0100

    Implement tile picking

--- Assets/Scripts/Presentation/World/SimulationManager.cs
+++ Assets/Scripts/Presentation/World/SimulationManager.cs
@@ -7,6 +7,10 @@ public class SimulationManager : MonoBehaviour {
 	public int WorldWidth = 10;
 	public int WorldHeight = 10;
 	public int Seed = 1337;
+
+	[SerializeField]
+	private Camera mainCamera;
+
 	public GameOverview GameOverview { get; private set; }
 
 	private void Awake() {
@@ -17,8 +21,21 @@ public class SimulationManager : MonoBehaviour {
 		GameOverview = new GameOverview(ExceptionHandler, Seed, WorldWidth, WorldHeight);
 	}
 
+	public void Update() {
+		Tile tile = GetClickedTile();
+		if (tile != null) Debug.Log(tile.Position);
+	}
+
 	public Projectile SpawnProjectile(Tower tower) {
 		throw new NotImplementedException();
+	}
+
+	private Tile GetClickedTile() {
+		if (!Input.GetMouseButtonDown(0)) return null;
+		Vector3 mousePosition = Input.mousePosition;
+		RaycastHit2D result = Physics2D.Raycast(mainCamera.ScreenToWorldPoint(mousePosition), Vector2.zero);
+
+		return !result ? null : result.collider.gameObject.GetComponent<Tile>();
 	}
 }
 }
