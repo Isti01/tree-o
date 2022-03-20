689ba02c6122d2d04e4787a8c89c6ae32fd651eb
Horváth István <dzctir@inf.elte.hu>
2022-03-20 20:54:17 +0000

    Implement the pause overlay (closes #29)

--- Assets/Scripts/Presentation/UI/BattleUI.cs
+++ Assets/Scripts/Presentation/UI/BattleUI.cs
@@ -1,5 +1,26 @@
+using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Presentation.UI {
-public class BattleUI : MonoBehaviour {}
+public class BattleUI : MonoBehaviour {
+	[SerializeField]
+	private UIDocument ui;
+
+	private VisualElement RootElement => ui.rootVisualElement;
+
+	private void Start() {
+		RootElement.Q<Button>("Pause").clicked += () => OnPauseClicked?.Invoke();
+	}
+
+	public void Show() {
+		RootElement.style.display = DisplayStyle.Flex;
+	}
+
+	public void Hide() {
+		RootElement.style.display = DisplayStyle.None;
+	}
+
+	public event Action OnPauseClicked;
+}
 }
--- Assets/Scripts/Presentation/UI/PauseOverlay.cs
+++ Assets/Scripts/Presentation/UI/PauseOverlay.cs
@@ -1,5 +1,30 @@
+using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Presentation.UI {
-public class PauseOverlay : MonoBehaviour {}
+public class PauseOverlay : MonoBehaviour {
+	[SerializeField]
+	private UIDocument ui;
+
+	private VisualElement RootElement => ui.rootVisualElement;
+
+	private void Start() {
+		RootElement.Q<Button>("ResumeButton").clicked += () => OnResumeClicked?.Invoke();
+		RootElement.Q<Button>("NewGameButton").clicked += () => OnNewGameClicked?.Invoke();
+		RootElement.Q<Button>("ExitButton").clicked += () => OnExitClicked?.Invoke();
+	}
+
+	public void Show() {
+		RootElement.style.display = DisplayStyle.Flex;
+	}
+
+	public void Hide() {
+		RootElement.style.display = DisplayStyle.None;
+	}
+
+	public event Action OnResumeClicked;
+	public event Action OnNewGameClicked;
+	public event Action OnExitClicked;
+}
 }
