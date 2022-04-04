f97ea347a89546f8c28515e807769be2a0f4750d
Horváth István <dzctir@inf.elte.hu>
2022-04-04 11:13:43 +0200

    Rename fields to comply with the Unity naming conventions

--- Assets/Scripts/Presentation/World/World.cs
+++ Assets/Scripts/Presentation/World/World.cs
@@ -2,33 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Logic.Data.World;
+using Logic.Event;
 using Logic.Event.World.Tower;
 using Logic.Event.World.Unit;
 using UnityEngine;
 
 namespace Presentation.World {
 public class World : MonoBehaviour {
-	public GameObject Tile;
-	public GameObject Barrack;
-	public GameObject Obstacle;
-	public GameObject Tower;
-	public GameObject Castle;
-	public GameObject Unit;
+	public GameObject tilePrefab;
+	public GameObject barrackPrefab;
+	public GameObject obstaclePrefab;
+	public GameObject towerPrefab;
+	public GameObject castlePrefab;
+	public GameObject unitPrefab;
 
 	private GameObject[,] _map;
 	private Dictionary<Logic.Data.World.Unit, Unit> _units;
-
-	public T LogicToPresentation<T>(TileObject tileObject) where T : Structure
-		=> _map[tileObject.Position.X, tileObject.Position.Y].GetComponentInChildren<T>();
-
-	public Unit LogicToPresentation(Logic.Data.World.Unit unit) => _units[unit];
 
 	private void Start() {
 		var simulation = FindObjectOfType<SimulationManager>();
 
 		_units = new Dictionary<Logic.Data.World.Unit, Unit>();
 
-		var events = simulation.GameOverview.Events;
+		EventDispatcher events = simulation.GameOverview.Events;
 		events.AddListener<TowerBuiltEvent>(OnTowerBuilt);
 		events.AddListener<TowerShotEvent>(OnTowerShot);
 		events.AddListener<TowerCooledDownEvent>(OnTowerCooledDown);
@@ -48,6 +44,14 @@ public class World : MonoBehaviour {
 		}
 	}
 
+	public T LogicToPresentation<T>(TileObject tileObject) where T : Structure {
+		return _map[tileObject.Position.X, tileObject.Position.Y].GetComponentInChildren<T>();
+	}
+
+	public Unit LogicToPresentation(Logic.Data.World.Unit unit) {
+		return _units[unit];
+	}
+
 	private void OnTowerUpgraded(TowerUpgradedEvent e) {
 		var tower = LogicToPresentation<Tower>(e.Tower);
 		tower.SetData(e.Tower);
@@ -60,12 +64,10 @@ public class World : MonoBehaviour {
 		Debug.Log($"Removed Tower: {e.Tower}");
 	}
 
-	private void Update() {}
-
 	private void OnUnitDeployed(UnitDeployedEvent e) {
 		try {
 			Logic.Data.World.Unit unitData = e.Unit;
-			GameObject unit = Instantiate(Unit, gameObject.transform);
+			GameObject unit = Instantiate(unitPrefab, gameObject.transform);
 			unit.transform.localPosition = new Vector3(unitData.Position.X - 0.5f, unitData.Position.Y - 0.5f);
 			var unitComponent = unit.GetComponent<Unit>();
 			unitComponent.SetData(unitData);
@@ -98,8 +100,8 @@ public class World : MonoBehaviour {
 	}
 
 	private void OnTowerShot(TowerShotEvent e) {
-		Tower tower = LogicToPresentation<Tower>(e.Tower);
-		LineRenderer laserRenderer = tower.GetComponent<LineRenderer>();
+		var tower = LogicToPresentation<Tower>(e.Tower);
+		var laserRenderer = tower.GetComponent<LineRenderer>();
 		Transform target = LogicToPresentation(e.Target).transform;
 
 		Vector3[] positions = { tower.transform.position, target.position };
@@ -125,21 +127,21 @@ public class World : MonoBehaviour {
 	}
 
 	private void OnTowerCooledDown(TowerCooledDownEvent e) {
-		Tower tower = LogicToPresentation<Tower>(e.Tower);
+		var tower = LogicToPresentation<Tower>(e.Tower);
 		//Disable the laser renderer when the tower is ready to shoot again:
 		// we want to avoid activating a new laser pulse and then deactivating the old one
 		tower.GetComponent<LineRenderer>().enabled = false;
 	}
 
 	private GameObject InstantiateTower(GameObject parent, Logic.Data.World.Tower tower) {
-		GameObject structure = Instantiate(Tower, parent.transform);
+		GameObject structure = Instantiate(towerPrefab, parent.transform);
 		var towerComponent = structure.GetComponent<Tower>();
 		towerComponent.SetData(tower);
 		return structure;
 	}
 
 	private GameObject InstantiateTile(int x, int y, GameWorld world) {
-		GameObject tile = Instantiate(Tile, transform);
+		GameObject tile = Instantiate(tilePrefab, transform);
 		tile.transform.localPosition = new Vector3(x, y);
 		var tileComponent = tile.GetComponent<Tile>();
 		tileComponent.Position = new TilePosition(x, y);
@@ -154,13 +156,13 @@ public class World : MonoBehaviour {
 	private GameObject InstantiateStructure(GameObject parent, TileObject tileData) {
 		switch (tileData) {
 			case Logic.Data.World.Barrack barrack: {
-				GameObject structure = Instantiate(Barrack, parent.transform);
+				GameObject structure = Instantiate(barrackPrefab, parent.transform);
 				var barrackComponent = structure.GetComponent<Barrack>();
 				barrackComponent.SetData(barrack);
 				return structure;
 			}
 			case Logic.Data.World.Castle castle: {
-				GameObject structure = Instantiate(Castle, parent.transform);
+				GameObject structure = Instantiate(castlePrefab, parent.transform);
 				var barrackComponent = structure.GetComponent<Castle>();
 				barrackComponent.SetData(castle);
 				return structure;
@@ -170,7 +172,7 @@ public class World : MonoBehaviour {
 				return structure;
 			}
 			case Logic.Data.World.Obstacle obstacle: {
-				GameObject structure = Instantiate(Obstacle, parent.transform);
+				GameObject structure = Instantiate(obstaclePrefab, parent.transform);
 				return structure;
 			}
 			default:
