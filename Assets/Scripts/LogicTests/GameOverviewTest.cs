c9204ef695e786bcb132fce7cff5984dd9813fef
Sárközi Gergely János <cycss7@inf.elte.hu>
2022-04-10 16:05:21 +0200

    add GameOverview unit tests and integration tests

---
+++ Assets/Scripts/LogicTests/GameOverviewTest.cs
@@ -0,0 +1,29 @@
+﻿using Logic.Command;
+using Logic.Data;
+using NUnit.Framework;
+
+namespace LogicTests {
+
+public class GameOverviewTest {
+	[Test]
+	public void TestNoOpRound() {
+		GameOverview overview = GameTestUtils.CreateOverview();
+		Assert.AreEqual(GamePhase.Prepare, overview.CurrentPhase);
+		overview.AdvancePhase();
+		Assert.AreEqual(GamePhase.Fight, overview.CurrentPhase);
+		overview.AdvancePhase();
+		Assert.AreEqual(GamePhase.Prepare, overview.CurrentPhase);
+	}
+
+	[Test]
+	public void TestFightingPhaseHasLimitedTime() {
+		GameOverview overview = GameTestUtils.CreateOverview();
+		overview.AdvancePhase();
+		Assert.AreEqual(GamePhase.Fight, overview.CurrentPhase);
+		float deltaTime = overview.TimeLeftFromPhase * 1.1f;
+		overview.Commands.Issue(new AdvanceTimeCommand(overview, deltaTime));
+		Assert.AreEqual(GamePhase.Prepare, overview.CurrentPhase);
+	}
+}
+
+}
